ad6dbf5e9cbad95a241ddc2646a1a6af02658a3a
arthurpsribeiro <arthurpsribeiro@gmail.com>
2021-05-31 17:52:41 -0300

    exemplo 02 comando require

---
+++ node/02-require/exemplo-require.js
@@ -0,0 +1,9 @@
+// log1
+// const meuLog = require('./meu-log');
+// console.log(meuLog);
+// meuLog("Olá, enfermeira!");
+
+//log2
+const meuLog2 = require('./meu-log-2');
+console.log(meuLog2);
+meuLog2.meuLog("Olá, enfermeira de novo!")
---
+++ node/02-require/meu-log-2.js
@@ -0,0 +1,3 @@
+exports.meuLog = (mensagem) => {
+    console.log(`vc quer imprimir no console isso aqui: ${mensagem}`)
+};
