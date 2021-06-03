e766aa415250cf91e2cfc38334a54ac3167d9eae
arthurpsribeiro <arthurpsribeiro@gmail.com>
2021-06-03 15:25:09 -0300

    instalando express e nodemon

--- express-mongodb/rastro/app.js
+++ express-mongodb/rastro/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
-console.log(express);
-
 const app = express();
 
-console.log(app);
-
 app.listen(3000 /*porta*/, () => console.log('Servidor rodando na porta 3000'));
+
+//criando a 1a rota
+// rota '/'
+
+// get() define a rota e funçaõ que deve ser executada
+app.get('/' /*url da rota*/, (request, response) => {
+    console.log('Rota principal chamada...');
+    response.send('Servidor rodando, está tudo ok.');
+});
+
+app.get('/rastreador' /*url da rota*/, (request, response) => {
+    console.log('Rota principal chamada...');
+    response.send('Servidor rodando, está tudo ok.');
+});
