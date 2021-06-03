036de56d626b77b72140ca4219a6c5c2af45d267
arthurpsribeiro <arthurpsribeiro@gmail.com>
2021-06-03 17:03:59 -0300

    iniciando configuraçao do  mongoose

--- express-mongodb/rastro/app.js
+++ express-mongodb/rastro/app.js
@@ -1,5 +1,24 @@
 const express = require('express');
 
+//configuracao do mongoose
+const mongoose = require('mongoose');
+
+//para fazer conexão com o MongoDB, através do Mongoose
+let connect = mongoose.connect(
+    'mongodb://localhost:2707//rastro-dev', //string de conexão
+    { // objeto de configurações
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    });
+
+console.log(connect);
+
+//classe schema do mongoose permite definir a estrutura de uma coleção no MongoDB
+const schema = mongoose.Schema;
+
+console.log(typeof(schema)); // CONTINUA.... 
+
 const app = express();
 
 // use() permite configuras alguns recrusos do express
@@ -12,7 +31,7 @@ app.listen(3000 /*porta*/, () => console.log('Servidor rodando na porta 3000'));
 //criando a 1a rota
 // rota '/'
 
-// get() define a rota e funçaõ que deve ser executada
+// get() define a rota e função que deve ser executada
 app.get('/' /*url da rota*/, (request, response) => {
     console.log('Rota principal chamada...');
     response.send('Servidor rodando, está tudo ok.');
@@ -24,3 +43,4 @@ app.post('/rastreador', (request, response) => {
     console.log(request.body);
     response.send('OK');
 });
+
