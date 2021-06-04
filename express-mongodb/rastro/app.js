34f97a0b7f598015c9d1ee78b5de905112c32f82
arthurpsribeiro <arthurpsribeiro@gmail.com>
2021-06-04 14:32:09 -0300

    ajusteem app.post()

--- express-mongodb/rastro/app.js
+++ express-mongodb/rastro/app.js
@@ -3,20 +3,6 @@ const express = require('express');
 //configuracao do mongoose
 const mongoose = require('mongoose');
 
-// parametros para mongoose.connect()
-let connectUrl = 'mongodb://localhost:27017/rastro-dev'; //string de conexao
-// objeto de configuracoes
-let connectOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-};
-
-// para fazer conexão com o MongoDB, através do Mongoose
-let connect = mongoose.connect(connectUrl, connectOptions);
-   
-console.log(connect);
-
 // classe schema do mongoose permite definir a estrutura de uma coleção no MongoDB
 const Schema = mongoose.Schema;
 
@@ -27,13 +13,8 @@ const rastreadorSchema = Schema({
     cpfCliente: {type: String, required: true}
 });
 
-console.log(typeof(Schema));  
-
-// model() cria a coleção
+// model() 'cria' a coleção
 const Rastreador = mongoose.model('rastreadores'/*nome da coleção */, rastreadorSchema);
-console.log(typeof(Rastreador));
-
-mongoose.disconnect;
 
 const app = express();
 
@@ -42,45 +23,51 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
-app.listen(3000 /*porta*/, () => console.log('Servidor rodando na porta 3000'));
+app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
 
 // criando a 1a rota
 // rota '/'
 
 // get() define a rota e função que deve ser executada
-app.get('/' /*url da rota*/, (request, response) => {
+app.get('/', (request, response) => {
     console.log('Rota principal chamada...');
     response.send('Servidor rodando, está tudo ok.');
 });
 
 // criando rota para cadastrar rastreador 
 app.post('/rastreador', (request, response) => {
-    console.log('Rota /rastreador chamada');
-    console.log(request.body);
 
-    // validar o request.body
-
+    // criar o documento na coleção 'rastreadores'
     const rastreador = new Rastreador(request.body);
-    console.log(`rastreador: ${rastreador} | Constructor: ${rastreador.constructor.name}`);
 
-    //
+    // parametros para mongoose.connect()
+    let connectUrl = 'mongodb://localhost:27017/rastro-dev'; //string de conexao
+    // objeto de configuracoes
+    let connectOptions = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    };
 
-    // criar o documento na coleção rastreadores
-    const createResult = Rastreador.create(rastreador)
-        .then((result) => {
-            console.log(`Resultado do .then ${result}`);
-            console.log(result);
-            console.log(`Rastreador ${rastreador.codigoRastreador} cadastrado com sucesso.`)
+    mongoose.connect(connectUrl, connectOptions)
+    .then(() => { // esse result, caso seja passado como parâmetro, é um objeto Mongoose (objeto de conexão)
+        console.log('Conexao com MongoDB realizada.');
+        Rastreador.create(rastreador)
+        .then((result) => { // esse result é um documento do rastreador
+            mongoose.disconnect();
             response.status(200).send(result);
+            console.log(`Rastreador ${rastreador.codigoRastreador} cadastrado com sucesso.`)
         })
         .catch((error) => {
-            console.log(`Erro do Create ${error} | Constructor: ${error.constructor.name}`)
-            console.log(error);
-            console.log(`Erro ao cadastrar o rastreador: ${error}`);
-            response.status(500).send(`Erro ao cadastrar o rastreador: ${error}`);
+            mongoose.disconnect();
+            response.status(500).send(`Erro ao cadastrar o Rastreador: ${error}`);
+            console.log(`Erro ao cadastrar o Rastreador: ${error}`);
         });
-
-    console.log(typeof(createResult));
+    })
+    .catch((error) => {
+        response.status(500).send(`Erro ao conectar no banco MongoDB: ${error}`);
+        console.log(`Erro ao conectar no banco MongoDB: ${error}`);
+    });
 
 });
 
