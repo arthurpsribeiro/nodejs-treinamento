b08f25806166171622ba795dfcfb88917c3eb595
arthurpsribeiro <arthurpsribeiro@gmail.com>
2021-06-08 10:34:28 -0300

    api alterar dados do Rastreador

--- express-mongodb/rastro/controllers/rastreador.js
+++ express-mongodb/rastro/controllers/rastreador.js
@@ -1,57 +1,102 @@
 const mongoose = require("mongoose");
 
+const connectUrl = "mongodb://localhost:27017/rastro-dev"; //string de conexao
+const connectOptions = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+};
+
 module.exports = (app) => {
-    const RastreadorController = {
-        // método cadastrar vai atender a rota POST /rastreador
-        cadastrar(request, response) {
-            const Rastreador = app.models.rastreador;
+	const RastreadorController = {
+		// método cadastrar vai atender a rota POST /rastreador
+		cadastrar(request, response) {
+			const Rastreador = app.models.rastreador;
 
-            // criar o documento na coleção 'rastreadores'
-            const rastreador = new Rastreador(request.body);
+			// criar o documento na coleção 'rastreadores'
+			const rastreador = new Rastreador(request.body);
 
-            // parametros para mongoose.connect()
-            let connectUrl = "mongodb://localhost:27017/rastro-dev"; //string de conexao
-            // objeto de configuracoes
-            let connectOptions = {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                useCreateIndex: true,
-            };
+			mongoose
+				.connect(connectUrl, connectOptions)
+				.then(() => {
+					// esse result, caso seja passado como parâmetro, é um objeto Mongoose (objeto de conexão)
+					console.log("Conexao com MongoDB realizada.");
+					//grava um documento na coleção
+					Rastreador.create(rastreador)
+						.then((result) => {
+							// esse result é um documento do rastreador
+							mongoose.disconnect();
+							response.status(200).send(result);
+							console.log(
+								`Rastreador ${rastreador.codigoRastreador} cadastrado com sucesso.`
+							);
+						})
+						.catch((error) => {
+							mongoose.disconnect();
+							response
+								.status(500)
+								.send(`Erro ao cadastrar o Rastreador: ${error}`);
+							console.log(`Erro ao cadastrar o Rastreador: ${error}`);
+						});
+				})
+				.catch((error) => {
+					response
+						.status(500)
+						.send(`Erro ao conectar no banco MongoDB: ${error}`);
+					console.log(`Erro ao conectar no banco MongoDB: ${error}`);
+				});
+		},
 
-            mongoose
-                .connect(connectUrl, connectOptions)
-                .then(() => {
-                    // esse result, caso seja passado como parâmetro, é um objeto Mongoose (objeto de conexão)
-                    console.log("Conexao com MongoDB realizada.");
-                    //grava um documento na coleção
-                    Rastreador.create(rastreador)
-                        .then((result) => {
-                            // esse result é um documento do rastreador
-                            mongoose.disconnect();
-                            response.status(200).send(result);
-                            console.log(
-                                `Rastreador ${rastreador.codigoRastreador} cadastrado com sucesso.`
-                            );
-                        })
-                        .catch((error) => {
-                            mongoose.disconnect();
-                            response
-                                .status(500)
-                                .send(
-                                    `Erro ao cadastrar o Rastreador: ${error}`
-                                );
-                            console.log(
-                                `Erro ao cadastrar o Rastreador: ${error}`
-                            );
-                        });
-                })
-                .catch((error) => {
-                    response
-                        .status(500)
-                        .send(`Erro ao conectar no banco MongoDB: ${error}`);
-                    console.log(`Erro ao conectar no banco MongoDB: ${error}`);
-                });
-        },
-    };
-    return RastreadorController;
+		alterar(request, response) {
+			console.log("Rota PUT /rastreador chamada...");
+
+			const Rastreador = app.models.rastreador;
+
+			mongoose
+				.connect(connectUrl, connectOptions)
+				.then(() => {
+					// a função updateOne() altera um documento da coleção
+					Rastreador.updateOne(
+						{
+							// objeto com o criterio de busca do documento
+							codigoRastreador: request.body.codigoRastreador,
+						},
+						{
+							// objeto com os dados que devem ser atualizados
+							$set: {
+								placaVeiculo: request.body.placaVeiculo,
+								cpfCliente: request.body.cpfCliente,
+							},
+						}
+					)
+						.then((result) => {
+							console.log(result);
+
+							if (result.nModified > 0) {
+								mongoose.disconnect();
+								response.status(200).send("Rastreador alterado com sucesso");
+							} else {
+								mongoose.disconnect();
+								response
+									.status(404)
+									.send("Rastreador não localizado no cadastro");
+							}
+						})
+						.catch((error) => {
+							mongoose.disconnect();
+							response
+								.status(500)
+								.send(`Erro ao alterar o Rastreador: ${error}`);
+							console.log(`Erro ao alterar o Rastreador: ${error}`);
+						});
+				})
+				.catch((error) => {
+					response
+						.status(500)
+						.send(`Erro ao conectar no banco MongoDB: ${error}`);
+					console.log(`Erro ao conectar no banco MongoDB: ${error}`);
+				});
+		},
+	};
+	return RastreadorController;
 };
--- express-mongodb/rastro/routes/rastreador.js
+++ express-mongodb/rastro/routes/rastreador.js
@@ -1,20 +1,18 @@
 module.exports = (app) => {
-     // criando a 1a rota
-    // rota '/'
+	// criando a 1a rota
+	// rota '/'
 
-    // get() define a rota e função que deve ser executada
-    app.get('/', (request, response) => {
-        console.log('Rota principal chamada...');
-        response.send('Servidor rodando, está tudo ok.');
-    });
+	// get() define a rota e função que deve ser executada
+	app.get("/", (request, response) => {
+		console.log("Rota principal chamada...");
+		response.send("Servidor rodando, está tudo ok.");
+	});
 
-    // criando rota para cadastrar rastreador 
-    app.post('/rastreador', app.controllers.rastreador.cadastrar);
+	// criando rota para cadastrar rastreador
+	app.post("/rastreador", app.controllers.rastreador.cadastrar);
 
+	// criando rota para altarar o rastreador
+	// PUT
+	// rota /rastreador
+	app.put("/rastreador", app.controllers.rastreador.alterar);
 };
-
-
-
-
-
-   
